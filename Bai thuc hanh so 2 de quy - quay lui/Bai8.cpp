e89a95c7b64f76d20dafa947237d327e68e01544
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-03-26 17:00:23 +0700

    liet ke hoan vi theo quy lui

--- Bai thuc hanh so 2 de quy - quay lui/Bai8.cpp
+++ Bai thuc hanh so 2 de quy - quay lui/Bai8.cpp
@@ -1,1 +1,32 @@
-#include
+#include<bits/stdc++.h>
+#define size 100
+using namespace std;
+
+void list_config(int x[], int n) {
+    for(int i=0; i<n; i++){
+        cout << x[i];
+    }
+    cout << endl;
+}
+
+void Try(int k, int x[], int n) {
+    int dd[100];
+    for(int i = 1; i <= n; i++) {   // Duyet cac phan thuoc tap S = {1, 2, ..., n}
+        if(dd[i] == 0) {    // chap nhan duoc la chua duoc chon truoc do
+            x[k] = i;
+            if(k == n) {    // neu tim duoc phan tu cuoi thi in cau hinh
+                list_config(x, n);
+            } else {
+                dd[i] = 1;  // danh dau viec chon i cho x[k]
+                Try(k+1, x, n); 
+                dd[i] = 0;  // huy ghi nhan viec chon i cho x[k]
+            }
+        }
+    }
+}
+
+int main(){
+    int x[size], n, k = 0;
+    cout << "So phan tu cua tap S: "; cin >> n;
+    Try(k, x, n);
+}
