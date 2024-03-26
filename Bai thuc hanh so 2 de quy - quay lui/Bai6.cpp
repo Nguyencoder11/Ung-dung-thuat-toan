f0390dbe1739f98587e7611e4368067b2213d07d
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-03-26 17:27:05 +0700

    bai toan thap Ha Noi theo khu de quy

---
+++ Bai thuc hanh so 2 de quy - quay lui/Bai6.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void chuyen(int n, char a, char b, char c){
+    
+}
+
+int main(){
+    int n;
+    char a = 'A', b = 'B', c = 'C';
+    cout << "So dia: "; cin >> n;
+    chuyen(n, a, b, c); 
+}
