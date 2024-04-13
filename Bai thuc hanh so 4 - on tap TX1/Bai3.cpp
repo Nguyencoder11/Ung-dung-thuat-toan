4d3ca56f487e211a16957869f4d77fe1ce0d8f34
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-04-13 09:46:24 +0700

    update :memo:

--- Bai thuc hanh so 4 - on tap TX1/Bai3.cpp
+++ Bai thuc hanh so 4 - on tap TX1/Bai3.cpp
@@ -12,13 +12,80 @@ theo dõi thời gian chạy thuật toán.
 
 #include<bits/stdc++.h>
 using namespace std;
+#define max 3000
 
 // de quy cho phuong phap phan doan - quicksort
 void quicksort(int a[], int l, int r){
-    if(l>r) return;
+    if(l>=r) return;
+
+    int pivot = a[l + (r-l)/2];
+    int i=l, j=r;
+
+    while(i<=j){
+        while(a[i] < pivot){
+            i++;
+        }
+
+        while(a[j] > pivot){
+            j--;
+        }
+
+        if(i<=j){
+            int tmp=a[i];
+            a[i]=a[j];
+            a[j]=tmp;
+            i++; j--;
+        }
+
+        if(l<j) quicksort(a, l, j);
+        if(r>i) quicksort(a, i, r);
+    }
 }
 
+void printArr(int a[], int size){
+    for(int i=0; i<size; i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+void khuDeQuy(int a[], int l, int r){
+    stack<pair<int, int>> stack;
+    stack.push({l, r});
+
+    while(!stack.empty()){
+        int left = stack.top().first;
+        int right = stack.top().second;
+        stack.pop();
+
+
+        int pivot = a[left + (right-left)/2];
+        int i=left, j=right;
+
+        while(i<=j){
+            while(a[i] < pivot) i++;
+            while(a[j] > pivot) j--;
+
+            if(i<=j){
+                swap(a[i], a[j]);
+                i++; 
+                j--;
+            }
+        }
+
+        if(left < j) stack.push({left, j});
+        if(i<right) stack.push({i, right});
+    }
+}
 
 int main(){
+    int a[max];
+    srand(time(nullptr));
 
+    for(int i=0; i<max; i++){
+        a[i] = rand();
+    }
+    // quicksort(a, 0, max-1);
+    khuDeQuy(a, 0, max-1);
+    printArr(a, max);
 }
---
+++ Bai thuc hanh so 4 - on tap TX1/Bai4.cpp
@@ -0,0 +1,32 @@
+/*
+Cho danh sach b chứa thông tin về n chuyến bay của 1 hãng hàng không, thông 
+tin về mỗi chuyến bay gồm số hiệu chuyến bay (ví dụ: VN005), giá vé, số ghế ngồi.
+2
+- Thiết kế thuật toán A1 để hiển thị ra màn hình tất cả các chuyến bay có giá vé 
+trên 700000 bằng phương pháp đệ quy.
+- Thiết kế thuật toán A2 để tìm và trả về chuyến bay có giá vé thấp nhất trong danh 
+sách b bằng chiến lược chia để trị.
+- Thiết kế thuật toán A3 để liệt kê tất cả các phương án khác nhau để chọn ra 4 
+chuyến bay từ danh sách b bằng phương pháp quay lui.
+- Viết chương trình thực hiện.
+o Khởi tạo số nguyên dương n sao cho 6 ≤ n ≤ 10. Khởi tạo danh sách b gồm 
+từ n chuyến bay, sao cho các chuyến bay có giá vé khác nhau đôi một
+(không nhập dữ liệu từ bàn phím, dữ liệu có tính thực tiễn).
+o Cài đặt các hàm biểu diễn các thuật toán A1, A2, A3.
+o Áp dụng thuật toán A1 để hiển thị ra màn hình tất cả các chuyến bay trong 
+danh sách b có giá vé trên 700000.
+o Áp dụng thuật toán A2 để tìm chuyến bay có giá vé thấp nhất trong danh 
+sách b, hiển thị thông tin đầy đủ của chuyến bay tìm được.
+o Áp dụng thuật toán A3 để hiển thị ra màn hình tất cả các phương án chọn 
+ra 4 chuyến bay từ danh sách b, mỗi chuyến bay cần hiển thị số hiệu 
+chuyến bay và số ghế ngồi.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+    
+}
