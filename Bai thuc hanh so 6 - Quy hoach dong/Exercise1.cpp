a5cc9c6ee196b1a44ff50f7d3007576e82141c0d
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-05-15 23:19:55 +0700

    :memo: update file code for Dynamic Programming

---
+++ Bai thuc hanh so 6 - Quy hoach dong/Exercise1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+/*
+Input: Cho số tự nhiên n ≤100.
+Output: Tính p là số cách phân tích số n thành tổng của dãy các số nguyên dương.
+Yêu cầu thực hiện:
+- Thiết kế thuật toán Q1 để tính và trả về p bằng chiến lược quy hoạch động.
+- Cài đặt chương trình ứng dụng:
+o Cài đặt hàm biểu diễn thuật toán Q1.
+o Khởi tạo n.
+o Sử dụng thuật toán Q1 để tính và đưa ra p.
+*/
+
+int main(){
+
+}
