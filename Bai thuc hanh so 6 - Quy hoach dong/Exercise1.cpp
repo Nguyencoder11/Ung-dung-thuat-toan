73b618bdf2bf6a59c82aebffaec5fbae736914a3
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-05-24 22:15:40 +0700

    phan tich so thanh tong

--- Bai thuc hanh so 6 - Quy hoach dong/Exercise1.cpp
+++ Bai thuc hanh so 6 - Quy hoach dong/Exercise1.cpp
@@ -12,6 +12,30 @@ o Khởi tạo n.
 o Sử dụng thuật toán Q1 để tính và đưa ra p.
 */
 
-int main(){
+int countPartitions(int n){
+    // khoi tao mang dp
+    vector<int> dp(n+1, 0);
+    dp[0] = 1;
 
+    // cap nhat mang dp
+    for(int i=1; i<=n; i++){
+        for(int j=i; j<=n; j++){
+            dp[j] += dp[j-i];
+        }
+    }
+
+    // so cach phan tich so n la dp[n]
+    return dp[n];
+}
+
+int main(){
+    int n;
+    cout << "Nhap so tu nhien n: "; cin >> n;
+    if(n<=0 || n>100){
+        cout << "n phai la so tu nhien tu 1 den 100.";
+        return 1;
+    }
+
+    int p = countPartitions(n);
+    cout << "So cach phan tich " << n << " thanh tong cac so nguyen duong: " << p << endl;
 }
