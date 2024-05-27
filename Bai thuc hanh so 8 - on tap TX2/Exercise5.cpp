dfebb7b3e220eb03dd2111e3d087861f2a475e09
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-05-27 21:41:31 +0700

    :art: xu ly xau ky tu

---
+++ Bai thuc hanh so 8 - on tap TX2/Exercise5.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+Input:
+- Cho danh sách d gồm n chiếc laptop thuộc các hãng sản xuất khác nhau, thông tin về
+mỗi chiếc laptop gồm tên hãng sản xuất, cấu hình, giá bán (ví dụ: HP, CPU 2.5GHz 
+upto 3.5GHz-RAM 16GB-SSD 512GB, 15000000; ACER, CPU 2.5GHz upto 
+3.5GHz-RAM 8GB-HDD 2TB, 12000000).
+Output:
+- Tính r là lượng số máy tính trong danh sách d có thông tin bộ nhớ trong là “RAM 
+16GB”.
+- Tính s và t lần lượt là số lượng và danh sách các máy tính trong danh sách d sử dụng 
+ổ cứng loại “SSD”.
+Cài đặt chương trình thực hiện:
+- Khai báo cấu trúc dữ liệu, khởi tạo n và danh sách d.
+- Cài đặt hàm F1 biểu diễn thuật toán Boyer Moore Horspool cho biết vị trí xuất hiện 
+đầu tiên của một xâu P trong một xâu T.
+- Cài đặt hàm F2 biểu diễn thuật toán Z cho biết một xâu P có xuất hiện trong một xâu 
+T hay không?
+- Khởi tạo n và danh sách d.
+- Cài đặt hàm F3 sử dụng hàm F1 để tính và trả về r.
+- Cài đặt hàm F4 sử dụng hàm F2 để tính và trả về s và t.
+- Sử dụng hàm F3 để tính và trả về r. Thông báo kết quả.
+- Sử dụng hàm F4 để tính và trả về s và t. Thông báo kết quả
+*/
+
+int main(){
+
+}
