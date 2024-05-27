1ed493c6aee4135fbdc31b4e64a8dc3f75dd2774
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-05-27 15:30:04 +0700

    chien luoc tham lam

--- Bai thuc hanh so 8 - on tap TX2/Exercise2.cpp
+++ Bai thuc hanh so 8 - on tap TX2/Exercise2.cpp
@@ -4,19 +4,19 @@ using namespace std;
 /*
 Input:
 - Cho một số tiền p và một danh sách d gồm n chiếc quạt bàn khác nhau về giá bán, 
-thông tin về mỗi chiếc quạt bàn gồm: tên hãng sản xuất, màu sắc và giá bán.
+thông tin về mỗi chiếc quạt bàn gồm: tên hãng sản xuất, màu sắc, giá bán.
 Output:
-- Tính c và s lần lượt là số lượng và danh sách nhiều nhất các quạt bàn có thể mua được 
-từ danh sách d với số tiền p.
-Yêu cầu thực hiện: 
-- Thiết kế thuật toán G1 để tính và trả về c và s theo chiến lược tham lam.
+- Tính u và v lần lượt là số lượng và danh sách ít nhất các quạt bàn trong danh sách d 
+cần bán để được một số tiền đúng bằng p.
+Yêu cầu thực hiện:
+- Thiết kế thuật toán G2 để tính và trả về u và v theo chiến lược tham lam.
 - Viết chương trình thực hiện:
 o Khai báo cấu trúc dữ liệu của bài toán.
 o Khởi tạo p (0 < p < 1200000), n và danh sách d (5 < n < 12). Yêu cầu dữ liệu 
 thể hiện được tính thực tiễn.
-o Cài đặt hàm G1 biểu diễn thuật toán G1.
-o Sử dụng thuật toán G1 để tính và trả về c và s. Hiển thị các kết quả, trong đó 
-mỗi chiếc quạt cần hiển thị tên hãng sản xuất và giá bán
+o Cài đặt hàm F2 biểu diễn thuật toán G2.
+o Sử dụng thuật toán G2 để tính và trả về u và v. Hiển thị các kết quả, trong đó 
+mỗi chiếc quạt cần hiển thị tên hãng sản xuất và giá bán.
 */
 
 struct Fan{
@@ -26,23 +26,27 @@ struct Fan{
 };
 
 bool comparePrice(const Fan &a, const Fan &b){
-    return a.price < b.price;
+    return a.price > b.price;
 }
 
-// Ham cai dat thuat toan G1
-pair<int, vector<Fan>> greedyAlgorithm(int p, vector<Fan> &d){
-    sort(d.begin(), d.end(), comparePrice);
+pair<int, vector<Fan>> greedyAlgorithm(int p, vector<Fan> &fans){
+    sort(fans.begin(), fans.end(), comparePrice);
 
-    int total_price = 0;    
-    vector<Fan> selectedItem;   // mang danh dau cac quat tran duoc chon
+    int total_price = 0;
+    vector<Fan> selectedItem;
 
-    for(Fan fan : d){
+    for(Fan fan : fans){
         if(total_price + fan.price <= p){
             selectedItem.push_back(fan);
             total_price += fan.price;
-        }else{
-            break;
+            if(total_price == p){
+                break;
+            }
         }
+    }
+
+    if(total_price != p){
+        selectedItem.clear();
     }
 
     return {selectedItem.size(), selectedItem};
@@ -57,20 +61,20 @@ int main(){
         {"BrandC", "Green", 120000},
         {"BrandD", "Yellow", 90000},
         {"BrandE", "Black", 200000},
-        {"BrandF", "White", 130000},
+        {"BrandF", "White", 300000},
         {"BrandG", "Pink", 170000},
-        {"BrandH", "Orange", 80000},
+        {"BrandH", "Orange", 500000},
         {"BrandI", "Purple", 160000},
         {"BrandJ", "Brown", 110000}
     };
 
     auto result = greedyAlgorithm(p, d);
-    int c = result.first;
-    vector<Fan> s = result.second;
+    int u = result.first;
+    vector<Fan> v = result.second;
 
-    cout << "So luong quat ban co the mua: " << c << endl;
+    cout << "So luong quat ban co the mua: " << u << endl;
     cout << "Danh sach cac quat ban co the mua:" << endl;
-    for(Fan fan : s){
+    for(Fan fan : v){
         cout << "Brand: " << fan.brand << ", Price: " << fan.price << endl;
     }
 }
