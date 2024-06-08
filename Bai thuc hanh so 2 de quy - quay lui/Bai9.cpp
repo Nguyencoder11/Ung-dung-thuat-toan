b34de7d8074f8b1113b7deb42be9079933b96c3e
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-06-08 11:17:40 +0700

    :memo: bai toan ma di tuan

--- Bai thuc hanh so 2 de quy - quay lui/Bai9.cpp
+++ Bai thuc hanh so 2 de quy - quay lui/Bai9.cpp
@@ -10,29 +10,37 @@ int board[n][n] = {0};
 void printBoard(){
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            cout << board[i][j] << "  ";
+            cout << board[i][j] << "\t";
         }
-        cout << endl;
+        cout << "\n\n\n";
     }
 }
 
 // kiem tra nuoc di tiep theo co ton tai tren ban co hay khong
-bool isValidMove(int *board, int n){
-
+bool isValidMove(int x, int y){
+    return (x >= 0 && x < n && y >= 0 && y < n && board[x][y] == 0);
 }
 
 // su dung backtracking de tim duong di cua quan ma
-void Try(int *board, int *x, int *y, int &u, int &v, int n){
+bool solveKTUtil(int u, int v, int movei, int x[], int y[]){
     int next_x, next_y;
-
+    if(movei == n*n) return true;
 
     // Thu tat ca cac buoc di cua quan ma
     for(int i=0; i<8; i++){
         next_x = u + x[i];
         next_y = v + y[i];
 
-        
+        if(isValidMove(next_x, next_y)){
+            board[next_x][next_y] = movei;
+            if(solveKTUtil(next_x, next_y, movei+1, x, y) == true){
+                return true;
+            }else{
+                board[next_x][next_y] = 0;
+            }
+        }
     }
+    return false;
 }
 
 int main(){
@@ -44,5 +52,10 @@ int main(){
     int y[8] = {1, 2,  2,  1, -1, -2, -2, -1};
 
     board[u][v] = 1;
-    printBoard();
+
+    if(solveKTUtil(u,v,2,x,y) == false){
+        cout << "Doesn't have solutions" << endl;
+    }else{
+        printBoard();
+    }
 }
